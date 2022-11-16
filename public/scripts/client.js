ef08a4568bc9cc92f077b8dd142deea14e866549
Sheri Lu <sherisherilu@gmail.com>
2022-11-16 04:11:13 +0000

    Edit client.js for dynamic tweets, change font weight in layout

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,80 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+// Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (let tweet of tweets) {
+        let $tweet = createTweetElement(tweet);
+        console.log($tweet)
+        $('#tweets-container').append($tweet)
+    }
+  }
+  
+
+
+  const createTweetElement = tweetInput => {
+    //return a tweet <article> element with entire HTML
+    let $tweet = $(`<article class="tweet">
+
+    <header class="tweet-header">
+        <div class="user-profile">
+          <img class="user-avatar" src="${tweetInput.user.avatars}"></img>
+          <h4 class="user-name">${tweetInput.user.name}</h4>
+        </div>
+        <div class="user-id">
+          ${tweetInput.user.handle}
+        </div>
+      </header>
+        <div class="tweet-text">
+          ${tweetInput.content.text}
+        </div>
+        
+        <footer class="tweeter-footer">
+          <span class="date"> ${tweetInput.created_at} </span>
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+
+    </article>`);
+
+    return $tweet;
+  };
+
+
+
+  renderTweets(data);
+
+})
