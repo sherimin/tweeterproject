e8a5a0f2a3087dba92b3fc8f2d45fdb8bf63cf46
Sheri Lu <sherisherilu@gmail.com>
2022-11-17 22:01:59 +0000

    Add Form Validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,36 +7,46 @@
 
 $(document).ready(function() {
 // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
   //Create an Ajax POST request
   $('#new-tweet-form').submit(function( event ) {
     //prevent the default submission behaviour
     event.preventDefault();
 
+    const maxCharacters = 140;
+    const inputLength = $(this).find("#tweet-text").val().length;
+
+    if (!inputLength) {
+      return alert("No tweet content.")
+    }
+
+    if (inputLength > maxCharacters) {
+      return alert("Your tweet is too long.")
+    }
     const tweet = $(this).serialize();
     $.post("/tweets/", tweet);
   })
