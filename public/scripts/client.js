91ea7d1fe5ccd5aa199fd91c4f86b54349ccb32b
Sheri Lu <sherisherilu@gmail.com>
2022-11-18 00:10:32 +0000

    rewrite loadTweets function with AJAX, edit sections in index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,6 @@
 
 
 $(document).ready(function() {
-// Fake data taken from initial-tweets.json
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
 
   //Create an Ajax POST request
   $('#new-tweet-form').submit(function( event ) {
@@ -53,16 +28,17 @@ $(document).ready(function() {
 
   //Receive array of tweets (JSON)
   const loadTweets = () => {
-    $.get("/tweets/", function(newTweets) {
-      renderTweets(newTweets.reverse());
+    $.ajax({
+      url:"/tweets",
+      method:"GET",
     })
+    .then((newTweets) => {
+      renderTweets(newTweets);
+    })
+
   };
 
-  loadTweets();
-
-
-
-
+ 
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -107,9 +83,8 @@ $(document).ready(function() {
     return $tweet;
   };
 
-
-
-  renderTweets(data);
+  //renderTweets(data);
+  loadTweets();
 
 });
 
